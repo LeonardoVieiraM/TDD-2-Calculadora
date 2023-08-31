73fbeb1c93e26fc31a1bd73a678cc22fc7fbfbb4
Guilherme Leão <guilherme.leao2001@hotmail.com>
2023-08-30 21:09:01 -0300

    Implementacao classe Subtracao

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -3,5 +3,14 @@ package Calculadora.src;
 public class Calculadora {
     public int multiplicacao(int a, int b) {
         return a * b;
-    } 
+    }
+
+    public int Subtracao(int a, int b){
+        return a-b;
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
 }
